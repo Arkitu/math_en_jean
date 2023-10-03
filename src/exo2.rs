bdc48700556c79dcae3c6db7fa7ffad11af32b9b
Arkitu <85173315+Arkitu@users.noreply.github.com>
2023-10-03 21:09:30 +0200

    ça thread fort

--- src/exo2.rs
+++ src/exo2.rs
@@ -23,27 +23,62 @@ fn evaluate_grid(grid: &Vec<i8>, size: usize) -> bool {
     true
 }
 
-fn compute_line(grid: &mut Vec<i8>, size: usize, i: usize) -> bool {
+// nombre de recursion après lequel on crée un nouveau thread
+const THREAD_RECURSION_COUNT: usize = 15;
+
+#[async_recursion::async_recursion]
+async fn compute_line(grid: &mut Vec<i8>, size: usize, i: usize) -> bool {
+    let new_treads = i % THREAD_RECURSION_COUNT == THREAD_RECURSION_COUNT-1;
     let base_value = grid.get(i).unwrap().to_owned();
+    let mut threads = if new_treads {
+        Some(Vec::new())
+    } else {None};
     for val in -1..=1 {
-        *grid.get_mut(i).unwrap() = val;
-        if i > 0 {
-            if compute_line(grid, size, i-1) {
-                return true
-            }
+        if new_treads {
+            let mut grid = grid.clone();
+            threads.as_mut().unwrap().push(
+                tokio::spawn(async move {
+                    *grid.get_mut(i).unwrap() = val;
+                    if i > 0 {
+                        if compute_line(&mut grid, size, i-1).await {
+                            return true
+                        }
+                    } else {
+                        if evaluate_grid(&grid, size) {
+                            return true
+                        }
+                    }
+                    false
+                })
+            )
         } else {
-            if evaluate_grid(grid, size) {
-                return true
+            *grid.get_mut(i).unwrap() = val;
+            if i > 0 {
+                if compute_line(grid, size, i-1).await {
+                    return true
+                }
+            } else {
+                if evaluate_grid(grid, size) {
+                    return true
+                }
             }
         }
+    }
+    match threads {
+        Some(t) => for t in t {
+            if t.await.unwrap() {
+                return true
+            }
+        },
+        None => {}
     }
     *grid.get_mut(i).unwrap() = base_value;
     false
 }
 
-fn compute_grid(size: usize) -> bool {
+async fn compute_grid(size: usize) -> bool {
     let mut grid: Vec<i8> = vec![-1; size.pow(2)];
-    let result = compute_line(&mut grid, size, size.pow(2)-1);
+    let result = compute_line(&mut grid, size, size.pow(2)-1).await;
     dbg!(Grid::from_vec(grid, size));
     result
 }
@@ -53,5 +88,5 @@ pub async fn main(args: &[String]) {
         .expect("No size gived !")
         .parse::<usize>()
         .expect("Invalid size !");
-    dbg!(compute_grid(size));
+    dbg!(compute_grid(size).await);
 }
