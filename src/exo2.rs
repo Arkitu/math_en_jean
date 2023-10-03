60b5c8c8cb3cec3c21022473536e2c2c652552a9
Arkitu <85173315+Arkitu@users.noreply.github.com>
2023-10-03 16:49:13 +0200

    ça marche pour le 2 omg

--- src/exo2.rs
+++ src/exo2.rs
@@ -1,33 +1,53 @@
 use std::collections::HashSet;
 
-use grid::Grid;
-
-fn evaluate_grid(grid: &Grid<i8>) -> bool {
+fn evaluate_grid(grid: &Vec<i8>, size: usize) -> bool {
     let mut set = HashSet::new();
-    for row in grid.iter_rows() {
-        let x = row.map(|x|*x).reduce(|a,b| a+b).unwrap();
+    let mut cols = vec![0; size];
+    for row in grid.chunks(size) {
+        for (i, a) in row.iter().enumerate() {
+            *cols.get_mut(i).unwrap() += a;
+        }
+        let x = row.iter().map(|x|*x).reduce(|a,b| a+b).unwrap();
         if set.contains(&x) {
             return false
         }
         set.insert(x);
     }
-    for col in grid.iter_cols() {
-        let x = col.map(|x|*x).reduce(|a,b| a+b).unwrap();
-        if set.contains(&x) {
+    for col in cols {
+        if set.contains(&col) {
             return false
         }
-        set.insert(x);
+        set.insert(col);
     }
     true
 }
 
-fn compute_grid(size: usize) {
-    let mut grid = Grid::init(size, size, -1);
-    for i in 0..(size^2) {
-        grid.get_mut(i /, col)
+fn compute_line(grid: &mut Vec<i8>, size: usize, i: usize) -> bool {
+    let base_value = grid.get(i).unwrap().to_owned();
+    for val in -1..=1 {
+        *grid.get_mut(i).unwrap() = val;
+        if i > 0 {
+            if compute_line(grid, size, i-1) {
+                return true
+            }
+        } else {
+            if evaluate_grid(grid, size) {
+                return true
+            }
+        }
     }
+    *grid.get_mut(i).unwrap() = base_value;
+    false
+}
+
+fn compute_grid(size: usize) -> bool {
+    let mut grid: Vec<i8> = vec![-1; size.pow(2)];
+    let result = compute_line(&mut grid, size, size.pow(2)-1);
+    dbg!(grid);
+    result
 }
 
 pub async fn main() {
-    
+    unsafe { backtrace_on_stack_overflow::enable() };
+    dbg!(compute_grid(7));
 }
